0b8521b0eea2d386adc2259d78fd3bda41be6ba8
wanhar <wanhardaengmari@gmail.com>
2021-05-07 14:37:33 +0700

    Initial Commit

---
+++ News App Modular/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News App Modular
+//
+//  Created by Wanhar on 07/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
